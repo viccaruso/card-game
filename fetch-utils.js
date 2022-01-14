d49f064ea303823b827bfcf09a188545e77d9a5a
cliffmax85 <cliffmax85@gmail.com>
2022-01-14 14:51:11 -0800

    added html pages

--- fetch-utils.js
+++ fetch-utils.js
@@ -16,7 +16,7 @@ export async function checkAuth() {
 
 export async function redirectIfLoggedIn() {
     if (await getUser()) {
-        location.replace('./other-page');
+        location.replace('./game');
     }
 }
 
