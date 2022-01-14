cfc439414f4034b510874ff9642c27cfb07c0b94
Victor Caruso <vic.caruso@gmail.com>
2022-01-14 12:34:19 -0800

    set up deck.js to export array for deck of cards

---
+++ deck.js
@@ -0,0 +1,314 @@
+export const deck = [
+    {
+        name: 'two-of-spades',
+        suit: 'hearts',
+        value: 2,
+        id: 1
+    },
+    {
+        name: 'three-of-spades',
+        suit: 'spades',
+        value: 3,
+        id: 2
+    },
+    {
+        name: 'four-of-spades',
+        suit: 'spades',
+        value: 4,
+        id: 3
+    },
+    {
+        name: 'five-of-spades',
+        suit: 'spades',
+        value: 5,
+        id: 4
+    },
+    {
+        name: 'six-of-spades',
+        suit: 'spades',
+        value: 6,
+        id: 5
+    },
+    {
+        name: 'seven-of-spades',
+        suit: 'spades',
+        value: 7,
+        id: 6
+    },
+    {
+        name: 'eight-of-spades',
+        suit: 'spades',
+        value: 8,
+        id: 7
+    },
+    {
+        name: 'nine-of-spades',
+        suit: 'spades',
+        value: 9,
+        id: 8
+    },
+    {
+        name: 'ten-of-spades',
+        suit: 'spades',
+        value: 10,
+        id: 9
+    },
+    {
+        name: 'jack-of-spades',
+        suit: 'spades',
+        value: 11,
+        id: 10
+    },
+    {
+        name: 'queen-of-spades',
+        suit: 'spades',
+        value: 12,
+        id: 11
+    },
+    {
+        name: 'king-of-spades',
+        suit: 'spades',
+        value: 13,
+        id: 12
+    },
+    {
+        name: 'ace-of-spades',
+        suit: 'spades',
+        value: 14,
+        id: 13
+    },
+    {
+        name: 'two-of-hearts',
+        suit: 'hearts',
+        value: 2,
+        id: 14
+    },
+    {
+        name: 'three-of-hearts',
+        suit: 'hearts',
+        value: 3,
+        id: 15
+    },
+    {
+        name: 'four-of-hearts',
+        suit: 'hearts',
+        value: 4,
+        id: 16
+    },
+    {
+        name: 'five-of-hearts',
+        suit: 'hearts',
+        value: 5,
+        id: 17
+    },
+    {
+        name: 'six-of-hearts',
+        suit: 'hearts',
+        value: 6,
+        id: 18
+    },
+    {
+        name: 'seven-of-hearts',
+        suit: 'hearts',
+        value: 7,
+        id: 19
+    },
+    {
+        name: 'eight-of-hearts',
+        suit: 'hearts',
+        value: 8,
+        id: 20
+    },
+    {
+        name: 'nine-of-hearts',
+        suit: 'hearts',
+        value: 9,
+        id: 21
+    },
+    {
+        name: 'ten-of-hearts',
+        suit: 'hearts',
+        value: 10,
+        id: 22
+    },
+    {
+        name: 'jack-of-hearts',
+        suit: 'hearts',
+        value: 11,
+        id: 23
+    },
+    {
+        name: 'queen-of-hearts',
+        suit: 'hearts',
+        value: 12,
+        id: 24
+    },
+    {
+        name: 'king-of-hearts',
+        suit: 'hearts',
+        value: 13,
+        id: 25
+    },
+    {
+        name: 'ace-of-hearts',
+        suit: 'hearts',
+        value: 14,
+        id: 26
+    },
+    {
+        name: 'two-of-diamonds',
+        suit: 'diamonds',
+        value: 2,
+        id: 27
+    },
+    {
+        name: 'three-of-diamonds',
+        suit: 'diamonds',
+        value: 3,
+        id: 28
+    },
+    {
+        name: 'four-of-diamonds',
+        suit: 'diamonds',
+        value: 4,
+        id: 29
+    },
+    {
+        name: 'five-of-diamonds',
+        suit: 'diamonds',
+        value: 5,
+        id: 30
+    },
+    {
+        name: 'six-of-diamonds',
+        suit: 'diamonds',
+        value: 6,
+        id: 31
+    },
+    {
+        name: 'seven-of-diamonds',
+        suit: 'diamonds',
+        value: 7,
+        id: 32
+    },
+    {
+        name: 'eight-of-diamonds',
+        suit: 'diamonds',
+        value: 8,
+        id: 33
+    },
+    {
+        name: 'nine-of-diamonds',
+        suit: 'diamonds',
+        value: 9,
+        id: 34
+    },
+    {
+        name: 'ten-of-diamonds',
+        suit: 'diamonds',
+        value: 10,
+        id: 35
+    },
+    {
+        name: 'jack-of-diamonds',
+        suit: 'diamonds',
+        value: 11,
+        id: 36
+    },
+    {
+        name: 'queen-of-diamonds',
+        suit: 'diamonds',
+        value: 12,
+        id: 37
+    },
+    {
+        name: 'king-of-diamonds',
+        suit: 'diamonds',
+        value: 13,
+        id: 38
+    },
+    {
+        name: 'ace-of-diamonds',
+        suit: 'diamonds',
+        value: 14,
+        id: 39
+    },
+    {
+        name: 'two-of-clubs',
+        suit: 'clubs',
+        value: 2,
+        id: 40
+    },
+    {
+        name: 'three-of-clubs',
+        suit: 'clubs',
+        value: 3,
+        id: 41
+    },
+    {
+        name: 'four-of-clubs',
+        suit: 'clubs',
+        value: 4,
+        id: 42
+    },
+    {
+        name: 'five-of-clubs',
+        suit: 'clubs',
+        value: 5,
+        id: 43
+    },
+    {
+        name: 'six-of-clubs',
+        suit: 'clubs',
+        value: 6,
+        id: 44
+    },
+    {
+        name: 'seven-of-clubs',
+        suit: 'clubs',
+        value: 7,
+        id: 45
+    },
+    {
+        name: 'eight-of-clubs',
+        suit: 'clubs',
+        value: 8,
+        id: 46
+    },
+    {
+        name: 'nine-of-clubs',
+        suit: 'clubs',
+        value: 9,
+        id: 47
+    },
+    {
+        name: '10-of-clubs',
+        suit: 'clubs',
+        value: 10,
+        id: 48
+    },
+    {
+        name: 'jack-of-clubs',
+        suit: 'clubs',
+        value: 11,
+        id: 49
+    },
+    {
+        name: 'queen-of-clubs',
+        suit: 'clubs',
+        value: 12,
+        id: 50
+    },
+    {
+        name: 'king-of-clubs',
+        suit: 'clubs',
+        value: 13,
+        id: 51
+    },
+    {
+        name: 'ace-of-clubs',
+        suit: 'clubs',
+        value: 14,
+        id: 52
+    }
+];
