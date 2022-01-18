78db01628ba1400d2871a768b61923d83a1b3fd1
TrevorRezac <trev.rezac@gmail.com>
2022-01-18 12:14:21 -0800

    created player and CPU deck state

--- game/game.js
+++ game/game.js
@@ -3,31 +3,65 @@ import { deck, shuffleDeck, splitDeck } from '../deck.js';
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
-const startGameButton = document.querySelector('.start-game');
+const newGameButton = document.querySelector('.new-game');
 const shuffleSound = document.querySelector('.soundBtn');
 const userCardCountEl = document.querySelector('.user-card-count');
 const cpuCardCountEl = document.querySelector('.cpu-card-count');
-
+const hitBtn = document.querySelector('.hit-btn');
 
 let userCardCount = 26;
 let cpuCardCount = 26;
+let playerDeck = [];
+let cpuDeck = [];
+
 
 window.addEventListener('load', async() => {
-
-});
-
-startGameButton.addEventListener('click', async() => {
     const hands = splitDeck(shuffleDeck(deck));
     console.log('The two hands are: ', hands);
     shuffleSound.play();
+
     userCardCount = 26;
     userCardCountEl.textContent = userCardCount;
     cpuCardCount = 26;
     cpuCardCountEl.textContent = cpuCardCount;
+
+    playerDeck = hands.playerDeck;
+    cpuDeck = hands.cpuDeck;
+    
+    console.log(playerDeck, cpuDeck);
+    
 });
 
+newGameButton.addEventListener('click', async() => {
+    const hands = splitDeck(shuffleDeck(deck));
+    console.log('The two hands are: ', hands);
+    shuffleSound.play();
 
+    userCardCount = 26;
+    userCardCountEl.textContent = userCardCount;
+    cpuCardCount = 26;
+    cpuCardCountEl.textContent = cpuCardCount;
+
+    playerDeck = hands.playerDeck;
+    cpuDeck = hands.cpuDeck;
+    
+    console.log(playerDeck, cpuDeck);
+    return playerDeck, cpuDeck;
+});
+
+hitBtn.addEventListener('click', async() => {
+    playGame();
+});
 
 logoutButton.addEventListener('click', () => {
     logout();
 });
+
+
+function playGame() {
+
+    // if (playerDeck[0].value > cpuDeck[0].value) {
+        
+    // }
+    console.log(playerDeck[5].value);
+}
